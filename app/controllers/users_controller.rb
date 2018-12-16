7b3b7fb93062ca5f6b341756616913c5cbe26d24
ajk0421 <ajk.spirituallounge@gmail.com>
2018-12-16 21:16:24 +0900

    add_new_create_action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,18 @@ class UsersController < ApplicationController
 
   def index
   end
-  
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    User.create(user_params)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name,:age)
+  end
 end
